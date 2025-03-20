cc0091208ca6953735286acc9bfbbbf12946ea5a
RHAIREN Pc <135435739+Adrriian@users.noreply.github.com>
2025-03-20 17:18:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,14 @@
- //* funcoes
-
- //* adicionar li
-  function adicionar(e){
-      if(e.key ==='Enter'){
-        const Nemli = document.createElement('li');
-        Nemli.innerHTML = input.value;
-        ul.appendChild(Nemli)
-
-        Nemli.addEventListener('click', abre);
-
-
-        input.value = '';
-      }
-
-    }
- 
-//* abrir o li
-
-  function abre(){
-    if(lista.style.display == 'flex'){
-        lista.style.display = 'none'
-    } else{
-        lista.style.display = 'flex'
-    }   
+let input = document.querySelector(".input")
+input.addEventListener("keyup", (e) => {
+  let input2 = document.querySelector(".input")
+  let list = document.querySelector(".list")
+  if (e.key.toLowerCase() === "enter") {
+    let li = document.createElement("li")
+    let span = document.createElement("span")
+    span.innerHTML = input2.value
+    li.innerHTML = '<input type = "checkbox" class="feito">' + '<span>'+ span.innerHTML +'</span>'
+    list.appendChild(li)
+    input.value = "";
   }
 
-//* fechar o li
-
-    function fecha(){ 
-      if(lista.style.display == 'none'){
-          lista.style.display = 'flex'
-      } else{
-          lista.style.display = 'none'
-      }
-      }
-        
-
-
-  //* elementos
-  const button = document.querySelector('button')
-  const lista = document.querySelector(".lista3");
-  const ul = document.querySelector('ul');
-  const input = document.querySelector('input');
-
-  //*Eventos
-  input.addEventListener('keyup', adicionar)
-  button.addEventListener('click', fecha)
+})
